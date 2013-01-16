8aeefd0490c8a645cbfb058eaab8e128cf2324b5
nickl- <github@jigsoft.co.za>
2013-01-16 11:30:28 +0200

    Minimalist sample Fabric settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+FABRIC = {
+    'HOSTS': ['localhost'],
+    'SSH_USER': 'ubuntu',
+    'SSH_PASS': 'aaa',
+    'DB_PASS' : 'aaa',
+    'ADMIN_PASS' : 'aaa',
+    'REPO_URL': 'git://github.com/tezro/mitimes.git',
+    'PROJECT_NAME': 'mitimes',
+}
+
+
+
